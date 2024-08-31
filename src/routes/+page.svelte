39453ac4d94c32942b1aa952c93bb6afab195125
Jacob Frost <jacobfrost@jacobs-mbp.lan>
2024-08-31 19:43:39 -0400

    Locally track games and hands

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { onMount } from 'svelte';
 
-    let step = 1;
+    let step = -1;
     let player = '';
     let venue = '';
     let date = '';
@@ -12,14 +12,47 @@
 	let positions = ['B', 'BB', 'SB', 'CO', 'HJ', 'LJ', 'UTG', 'UTG1'];
     const cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
     const players = [2, 3, 4, 5, 6, 7, 8, 9];
+    let handHistory = [];
+    let gameHistory = [];
 
     onMount(() => {
         const now = new Date();
         date = now.toISOString().split('T')[0]; // YYYY-MM-DD format
+
+        for (let i = 0; i < localStorage.length; i++) {
+            // Get the key at position i
+            const key = localStorage.key(i);
+            // Print the key to the console
+            gameHistory.push(key);
+            console.log(key);
+        }
+        step = 0;
     });
 
-    function confirmPlayerInfo() {
+    function confirmGameInfo() {
+        // create a local storage key
+        const key = `${venue} - ${date}`;
+        // create a local storage value that is json 
+        const value = JSON.stringify({ player, venue, date, handHistory });
+        // set the key value pair in local storage
+        localStorage.setItem(key, value);
+
+        gameHistory.push(key);
+
         step = 2;
+    }
+
+    function loadGame(key) {
+        const value = localStorage.getItem(key);
+        const gameDetails = JSON.parse(value);
+
+        console.log(gameDetails);
+
+        player = gameDetails.player;
+        venue = gameDetails.venue;
+        date = gameDetails.date;
+        handHistory = gameDetails.handHistory ?? [];
+        step = 4;
     }
 
     function handlePosition(pos) {
@@ -31,7 +64,6 @@
 
     function handlePlayers(p) {
         playerCount = parseInt(p);
-        // positions = positionList.slice(0, playerCount)
 		positions = [];
 		for (let i=0; i < Math.min(6, playerCount); i++){
             positions.push(positionList[i]);
@@ -51,10 +83,6 @@
             positions.push('UTG1');
             positions.push('UTG');
         };
-    }
-
-    function confirmPlayerPosition() {
-        step = 3;
     }
 
     function handleCardSelection(card) {
@@ -84,27 +112,44 @@
     }
 
     function confirmCardSelection() {
+        handHistory.push({
+            position: position,
+            playerCount: playerCount,
+            card1: currentHand.card1,
+            card2: currentHand.card2,
+            suited: currentHand.suited
+        });
+
+        // update the game in local storage
+        const key = `${venue} - ${date}`;
+        const value = JSON.stringify({ player, venue, date, handHistory });
+        localStorage
+            .setItem(key, value);
+
+        currentHand = { suited: false, card1: null, card2: null };
+
         step = 4;
+    }
+
+    function confirmPlayerPosition() {
+        step = 3;
+    }
+
+    function goToCardSelectionScreen() {
+        // if there is no position or player count selected, go back to the position and player selection screen
+        if (!position || !playerCount) {
+            step = 2;
+        }
+
+        step = 3;
     }
 
     function goToPositionAndPlayerScreen() {
         step = 2;
     }
-
-    function goToCardSelectionScreen() {
-        step = 3;
-    }
 </script>
 
 <style>
-    body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 20px;
-        /* background-color: #FF0000; */
-        background-color: #f4f4f4;
-    }
-
     .container {
         max-width: 900px;
         margin: auto;
@@ -112,12 +157,6 @@
         background: #D3D3D3;
         border-radius: 8px;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-
-    .grid-container {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-        gap: 20px;
     }
 
     .player-postion {
@@ -145,8 +184,7 @@
         align-items: center;
     }
 
-    .card-grid button:hover,
-    .card-grid button.active {
+    .card-grid button:hover{
         background-color: #007bff;
         color: #fff;
     }
@@ -162,8 +200,7 @@
         margin-bottom: 5px;
     }
 
-    .player-info input,
-    .player-info select {
+    .player-info input{
         padding: 10px;
         font-size: 16px;
         border: 1px solid #ccc;
@@ -223,10 +260,6 @@
         background-color: #007bff;
     }
 
-    .card-selection {
-        margin-bottom: 20px;
-    }
-
     .card-display {
         display: flex;
         flex-direction: column;
@@ -247,72 +280,9 @@
         margin-right: 10px;
     }
 
-    .card-info button {
-        margin-left: 10px;
-        padding: 5px 10px;
-        font-size: 14px;
-        border: 1px solid #007bff;
-        background-color: #fff;
-        color: #007bff;
-        border-radius: 4px;
-        cursor: pointer;
-    }
-
-    .card-info button:hover {
-        background-color: #007bff;
-        color: #fff;
-    }
-
     .card-display span {
         font-size: 24px;
         margin-right: 10px;
-    }
-
-    .card-display .undo-button {
-        margin-left: 20px;
-        background-color: #f8f9fa;
-        border: 1px solid #ccc;
-        border-radius: 50%;
-        width: 36px;
-        height: 36px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        cursor: pointer;
-    }
-
-    .card-display .undo-button:hover {
-        background-color: #e2e6ea;
-    }
-
-    .card-display .suited {
-        margin-top: 10px;
-        display: flex;
-        align-items: center;
-    }
-
-    .suited span {
-        margin-right: 10px;
-    }
-
-    .suited button {
-        padding: 10px 20px;
-        font-size: 16px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        background-color: #f8f9fa;
-        color: #007bff;
-        cursor: pointer;
-    }
-
-    .suited button.active {
-        background-color: #007bff;
-        color: #fff;
-    }
-
-    .suited button:hover {
-        background-color: #007bff;
-        color: #fff;
     }
 
     .suited-row {
@@ -375,7 +345,19 @@
 </style>
 
 <div class="container">
-    {#if step === 1}
+    {#if step === 0}
+    <div class="player-info">
+
+        {#each gameHistory as game}
+        <button class="card-info" on:click={() => loadGame(game)}>
+            {game}
+        </button>
+        {/each}
+
+        <button class="confirm-button" on:click={() => step = 1}>Add New Game</button>
+    </div>
+    {:else if step === 1}
+
         <div class="player-info">
             <h2>Player Information</h2>
             <label for="player">player:</label>
@@ -384,7 +366,7 @@
             <input id="venue" type="text" bind:value={venue} />
             <label for="date">Date:</label>
             <input id="date" type="date" bind:value={date} />
-            <button class="confirm-button" on:click={confirmPlayerInfo}>Confirm</button>
+            <button class="confirm-button" on:click={confirmGameInfo}>Confirm</button>
         </div>
     {:else if step === 2}
         <div class="player-postion">
@@ -450,14 +432,15 @@
         </div>
     {:else if step === 4}
         <div>
+            {#each handHistory as hand}
+
             <div class="card-info">
-                <div class="text-box" on:click={goToPositionAndPlayerScreen}>
-                    {position} | {playerCount}
-                </div>
-                <div class="text-box" on:click={goToCardSelectionScreen}>
-                    {currentHand.card1 ?? ' '}, {currentHand.card2 ?? ' '}, Suited: {currentHand.suited ? 'Yes' : 'No'}
+                <div class="text-box">
+                    Pos: {hand.position}  Count: {hand.playerCount}  C1: {hand.card1 ?? ' '} C2: {hand.card2 ?? ' '} Suited: {hand.suited ? 'Yes' : 'No'}
                 </div>
             </div>
+            {/each}
+            <button class="confirm-button" on:click={goToCardSelectionScreen}>Add Another Hand</button>
         </div>
     {/if}
 </div>
